8e900cbbcf17fa6943484169d0f639196e02142c
yang4916 <park49164357@gmail.com>
2024-08-11 07:36:23 +0900

    FEAT:JS작업(완료: 나의 인기상품, 주문 통계)(진행: 매출 캘린더)

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,68 +1,101 @@
+const today = new Date("2023-12-31");
+
 axios
   .get("http://localhost:5500/v4.json")
   .then((res) => {
-    let products = res.data;
+    const products = res.data;
     // 주문현황
     let orderToday = 0; // 신규 주문
     let saleToday = 0; // 금일 매출
-    for (let i = 0; i < products.length; i++) {
-      for (let j = 0; j < products[i].platforms.length; j++) {
-        for (let k = 0; k < products[i].platforms[j].sales.length; k++) {
-          if (products[i].platforms[j].sales[k].date == "2023-12-31") {
-            orderToday += products[i].platforms[j].sales[k].quantity;
-            saleToday +=
-              products[i].platforms[j].sales[k].quantity * products[i].price;
+    products.forEach((products) => {
+      products.platforms.forEach((platforms) => {
+        platforms.sales.forEach((sales) => {
+          if (today - new Date(sales.date) == 0) {
+            orderToday += sales.quantity;
+            saleToday += sales.quantity * products.price;
           }
-        }
-      }
-    }
-    let orderUnknown = Math.round(orderToday / 5); // 미확인 주문 (임시)
-    let orderKnown = Math.round(orderToday / 3); // 주문 확정 (임시)
-    let productsHTML = `
-      <h1 class="title">주문 현황<span>(당일 0시 기준)</span></h1>
+        });
+      });
+    });
+    const orderUnknown = Math.round(orderToday / 5); // 미확인 주문 (임시)
+    const orderKnown = Math.round(orderToday / 3); // 주문 확정 (임시)
+    const orderHTML = `
+      <h1 class="title">주문 현황<span> (당일 0시 기준)</span></h1>
       <p>
         <span>미확인 주문 ${orderUnknown.toLocaleString()}건</span>
         <span>신규 주문 ${orderToday.toLocaleString()}건</span>
         <span>주문 확정 ${orderKnown.toLocaleString()}건</span>
         <span>금일 매출 ${saleToday.toLocaleString()}원</span>
       </p>`;
-    document.querySelector("#main-order").innerHTML = productsHTML;
+    document.querySelector("#main-order").innerHTML = orderHTML;
+
+    // 나의 인기 상품
+    const platformSelect = document.getElementById("product-platform");
+
+    const printPopluarProduct = () => {
+      let poppularProduct = [];
+      products.forEach((products) => {
+        products.platforms.forEach((platforms) => {
+          platforms.sales.forEach((sales) => {
+            if (platforms.platform == platformSelect.value) {
+              if (today - new Date(sales.date) == 0) {
+                poppularProduct.push([products.name, sales.quantity]);
+              }
+            }
+          });
+        });
+      });
+      poppularProduct.sort((a, b) => b[1] - a[1]);
+
+      document.querySelector("#main-popular-product-list").innerHTML = `
+        <p class="list-title">
+          <span class="list-side">순위</span>
+          <span class="list-center">상품</span>
+          <span class="list-side">판매량</span>
+        </p>
+        `;
+      for (let i = 0; i < 5; i++) {
+        let popularProductHTML;
+        if (i > 0 && poppularProduct[i][1] == poppularProduct[i - 1][1]) {
+          popularProductHTML = `
+            <li>
+              <span class="list-side">${i}위</span>
+              <span class="list-center">${poppularProduct[i][0]}</span>
+              <span class="list-side">${poppularProduct[i][1]}건</span>
+            </li>
+            `;
+        } else {
+          popularProductHTML = `
+          <li>
+            <span class="list-side">${i + 1}위</span>
+            <span class="list-center">${poppularProduct[i][0]}</span>
+            <span class="list-side">${poppularProduct[i][1]}건</span>
+          </li>
+          `;
+        }
+        document.querySelector("#main-popular-product-list").innerHTML +=
+          popularProductHTML;
+      }
+    };
+    printPopluarProduct();
+    platformSelect.addEventListener("change", printPopluarProduct);
+
+    // 인기 해시태그
+    let printPopluarHashtag = () => {
+      let popularHashtagHTML = [];
+      document.querySelector("#main-hashtag-list").innerHTML = `
+         <p class="list-title">
+           <span class="list-side">순위</span>
+           <span class="list-center">상품</span>
+           <span class="list-side">판매량</span>
+         </p>
+         `;
+      document.querySelector("#main-hashtag-list").innerHTML +=
+        popularHashtagHTML;
+    };
+    printPopluarHashtag();
+    platformSelect.addEventListener("change", printPopluarHashtag);
   })
   .catch((error) => {
     console.log("error 발생 : " + error);
   });
-
-const chartOptions = {
-  responsive: true,
-  maintainAspectRatio: false,
-  scales: {
-    y: {
-      beginAtZero: true,
-    },
-  },
-  plugins: {
-    legend: {
-      display: false,
-    },
-  },
-};
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  const weeklyCountChart = new Chart(
-    document.getElementById("week-count").getContext("2d"),
-    {
-      type: "bar",
-      data: {
-        labels: ["12/25", "12/26", "12/27", "12/28", "12/29", "12/30", "12/31"],
-        datasets: [
-          {
-            label: "주문",
-            data: [150, 120, 58, 90, 76, 70, 58],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: chartOptions,
-    }
-  );
-});
